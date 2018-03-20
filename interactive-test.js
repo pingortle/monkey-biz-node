71d243def6cd0b5ecb9c8972c4b3f478d38efdce
Kaleb Lape <kaleb.lape@metova.com>
2018-03-20 15:30:55 -0500

    Replace sad winky mustache with sad mustache. (i.e. Remove semicolon)

--- interactive-test.js
+++ interactive-test.js
@@ -7,7 +7,7 @@ const input = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
-});
+})
 
 const socket = udp.createSocket('udp4')
 const pipe = new OpenPipe(socket)
